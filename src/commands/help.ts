9fbcbce327578c0e8038a4ebe922139cf722a0be
Extenv <im.ivan.maulana@gmail.com>
2025-11-11 14:32:36 +0700

    Update

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -11,5 +11,12 @@ export const helpCommand: Command = {
     console.log(
       'bunx zoc archive <version> - Archive document into versioned release'
     );
+    console.log('bunx zoc archive --blank - New Documentation without content');
+    console.log(
+      'bunx zoc archive --roll=<version> - Rolling back documentation to specific version '
+    );
+    console.log(
+      'bunx zoc archive --dell=<version> - Delete documentation specific version '
+    );
   },
 };
